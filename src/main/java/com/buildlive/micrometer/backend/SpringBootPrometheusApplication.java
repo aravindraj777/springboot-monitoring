6b823803010024821161e2c85ea5684140eb2127
Aravind <aravindmunna240@gmail.com>
2024-08-06 18:16:19 +0530

    prometheus integrated and visualised on grafana dashboard

--- src/main/java/com/buildlive/micrometer/backend/SpringBootPrometheusApplication.java
+++ src/main/java/com/buildlive/micrometer/backend/SpringBootPrometheusApplication.java
@@ -2,10 +2,17 @@ package com.buildlive.micrometer.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SpringBootPrometheusApplication {
 
+	@GetMapping
+	public String getStarterMessage(){
+		return "Started working";
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootPrometheusApplication.class, args);
 	}
